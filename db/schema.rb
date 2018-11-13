2f1de3485214ab7c785ee98a1d4000fe3f8b5825
gusarov2k <motoface@yandex.ru>
2018-11-13 18:22:29 +0300

    finish hasbtm

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181113144119) do
+ActiveRecord::Schema.define(version: 20181113150647) do
 
   create_table "carts", force: :cascade do |t|
     t.integer "user_id"
   end
+
+  create_table "carts_items", id: false, force: :cascade do |t|
+    t.integer "cart_id"
+    t.integer "item_id"
+  end
+
+  add_index "carts_items", ["cart_id", "item_id"], name: "index_carts_items_on_cart_id_and_item_id"
+  add_index "carts_items", ["item_id", "cart_id"], name: "index_carts_items_on_item_id_and_cart_id"
 
   create_table "items", force: :cascade do |t|
     t.string   "name"
