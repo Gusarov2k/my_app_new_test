35f1fb8d2e6a783c804da331f36c84c006fa43e0
gusarov2k <motoface@yandex.ru>
2018-11-13 15:59:14 +0300

    create migration

--- db/migrate/20181113124710_create_items.rb
+++ db/migrate/20181113124710_create_items.rb
@@ -2,6 +2,11 @@ class CreateItems < ActiveRecord::Migration
   def change
     create_table :items do |t|
 
+      t.string    :name
+      t.text      :description
+      t.float     :price
+      t.float     :weight
+      
       t.timestamps null: false
     end
   end
