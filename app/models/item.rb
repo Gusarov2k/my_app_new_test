f600ac8774990c73d4d8105f000b941810a3519c
gusarov2k <motoface@yandex.ru>
2018-11-13 16:12:35 +0300

    create validates

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,5 @@
 class Item < ActiveRecord::Base
+  # create validates
+  validates   :price, :weight,      { numericality: {greater_than: 0}, 
+                                    presence: true }
 end
