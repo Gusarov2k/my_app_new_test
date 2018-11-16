b6dd7486f25ccf26280ee1abbdb3ca5b6cc9660b
gusarov2k <motoface@yandex.ru>
2018-11-16 14:06:54 +0300

    add metod

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -4,4 +4,12 @@ class Cart < ActiveRecord::Base
 
   has_many :positions
   has_many :items, through: :positions
+
+  def add_item(i)
+    if items.include?(i)
+      positions.where(item_id: i.id).first.increment!(:quantity) 
+    else
+      positions.create(item: i, quantity: 1)
+    end
+  end
 end
